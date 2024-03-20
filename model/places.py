08086f49b38f17cbdfb6c3e7b5e6ca2fb234439d
shubhayy <shubhayc14@gmail.com>
2024-03-19 23:42:43 -0700

    Model from NB

--- model/places.py
+++ model/places.py
@@ -5,5 +5,42 @@ import imghdr
 import numpy as np
 from matplotlib import pyplot as plt
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers, models
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 
+data = '/Users/shubhay/Documents/GitHub/BackendTri3/places'
+data = tf.keras.utils.image_dataset_from_directory('/Users/shubhay/Documents/GitHub/BackendTri3/places')
 
+data_iterator = data.as_numpy_iterator()
+batch = data_iterator.next()
+data = data
+scaled_iterator = data.as_numpy_iterator()
+batch = scaled_iterator.next()
+
+train_size = int(len(data)*.7)
+val_size = int(len(data)*.2)
+test_data = int(len(data)*.1)
+
+train = data.take(train_size)
+val = data.skip(train_size).take(val_size)
+
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(512, activation='relu'),
+    layers.Dense(10, activation='softmax')  # 10 classes
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+hist = model.fit(train, epochs=30, validation_data=val,)
